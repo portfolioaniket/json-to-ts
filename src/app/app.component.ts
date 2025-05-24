451bb5204cff72b98055fcb0c843ca47d2ed2177
sanofixaniket <Aniket.Pandey@sanofi.com>
2025-05-24 11:49:22 +0530

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,38 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'jsonToTS';
+  jsonText = '';
+  tsText = ''
+  errorMessage!: any
+  hasError = false;
+  resultHeader = 'Output'
+  createTS(button: string) {
+    try {
+      const objFromJson = JSON.parse(this.jsonText)
+      this.resultHeader = button == 'TS' ? 'Typescript' : button === 'FORM' ? "Form Control" : 'Output'
+      this.tsText = button == 'TS' ? "{\n" : 'new FormGroup({\n'
+      for (let key in objFromJson) {
+        const type = button === 'TS' ? Array.isArray(objFromJson[key]) ? 'Array' : typeof objFromJson[key] : 'new FormControl()'
+        this.tsText = this.tsText + "\t" + key + " : " + type + '\n';
+      }
+      this.tsText += button == 'TS' ? "}\n" : '})\n'
+      console.log("cdsa", this.tsText)
+
+    } catch (error) {
+      this.hasError = true
+      this.errorMessage = error
+    }
+  }
+  closeModal() {
+    this.hasError = false
+  }
+  formBuilder() {
+    this.tsText = this.tsText.replace('new FormGroup', 'this.fb.group')
+    this.tsText = this.tsText.replaceAll('new FormControl()', '[]')
+  }
+  reset() {
+    this.jsonText = ''
+    this.tsText = ''
+    this.resultHeader = "Output"
+  }
 }
